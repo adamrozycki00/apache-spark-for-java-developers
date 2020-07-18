c5591cc924fd17ca54198acefabad031566530a0
Adam Rozycki <adamrozycki00@gmail.com>
2020-07-18 22:04:51 +0200

    Add an exercise of finding the most popular words in a text file

--- module-1-spark-core/src/main/java/com/virtualpairprogrammers/Main.java
+++ module-1-spark-core/src/main/java/com/virtualpairprogrammers/Main.java
@@ -4,44 +4,37 @@ import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.sparkproject.guava.collect.Iterables;
 import scala.Tuple2;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        System.setProperty("hadoop.home.dir", "C:/hadoop");
         Logger.getLogger("org.apache").setLevel(Level.WARN);
-
-        List<String> inputData = new ArrayList<>();
-        inputData.add("WARN: Tuesday 4 September 0405");
-        inputData.add("WARN: Tuesday 4 September 0406");
-        inputData.add("ERROR: Tuesday 4 September 0408");
-        inputData.add("FATAL: Wednesday 5 September 1632");
-        inputData.add("ERROR: Friday 7 September 1854");
-        inputData.add("WARN: Saturday 8 September 1942");
 
         SparkConf conf = new SparkConf()
                 .setAppName("startingSpark")
                 .setMaster("local[*]");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
-        sc.parallelize(inputData)
-                .mapToPair(str -> new Tuple2<>(str.split(":")[0], 1L))
+        String filePath = Main.class.getClassLoader().getResource("subtitles/input.txt").getFile();
+
+        sc.textFile(filePath)
+                .flatMap(str -> Arrays.asList(str.split(" ")).iterator())
+                .map(word -> word.replaceAll("[^a-zA-Z]", ""))
+                .map(String::toLowerCase)
+                .filter(word -> word.length() > 1)
+                .filter(Util::isNotBoring)
+                .mapToPair(word -> new Tuple2<>(word, 1L))
                 .reduceByKey(Long::sum)
-                .foreach(tuple -> System.out.println(tuple._1 + " has " + tuple._2 + " instances"));
-
-        sc.parallelize(inputData)
-                .mapToPair(str -> new Tuple2<>(str.split(":")[0], 1L))
-                .groupByKey()
-                .foreach(tuple -> System.out.println(tuple._1 + " has " + Iterables.size(tuple._2) + " instances"));
-
-        sc.parallelize(inputData)
-                .flatMap(str -> List.of(str.split(" ")).iterator())
-                .foreach(word -> System.out.println(word));
+                .mapToPair(tuple -> new Tuple2<>(tuple._2, tuple._1))
+                .sortByKey(false)
+                .take(10)
+                .forEach(System.out::println);
 
     }
+
 }
